cdd5359bdc32e260c5b74897eda60f923be1217c
Jonathan Fok kan <jfokkan@gmail.com>
2016-01-23 00:27:35 -0500

    Added wilks and partial input
    Partial input is save in case it contains a '.' such as "67."
    This input would be converted to a float correctly but will
    strip the "." from it and make it hard to enter such numbers
    in the field since the content of the field was controlled by
    the model. With the partial model we can now save a partial input
    that can eventually lead to a correct input.

--- main.elm
+++ main.elm
@@ -26,25 +26,29 @@ toUnits unit =
 
 type alias Model = {
     gender : Gender,
+    partialBodyweight: String,
     bodyweightUnits: Units,
     bodyweight: Maybe Float,
     weightUnits: Units,
+    partialWeight: String,
     weight: Maybe Float
   }
 
 initModel = { gender = Male,
               bodyweightUnits = Lbs,
+              partialBodyweight = "",
               bodyweight = Nothing,
               weightUnits = Lbs,
+              partialWeight = "",
               weight = Nothing }
 
 -- Update
 type Action =
   UpdateGender Gender |
   UpdateBodyweightUnits Units |
-  UpdateBodyweight (Maybe Float) |
+  UpdateBodyweight (Maybe Float) String |
   UpdateWeightUnits Units |
-  UpdateWeight (Maybe Float) |
+  UpdateWeight (Maybe Float) String |
   NoOp
 
 
@@ -53,22 +57,31 @@ update action model =
   case action of
     UpdateGender gender -> { model | gender = gender }
     UpdateBodyweightUnits unit -> { model | bodyweightUnits = unit }
-    UpdateBodyweight weight -> { model | bodyweight = weight }
+    UpdateBodyweight weight partial -> { model | bodyweight = weight, partialBodyweight = partial }
     UpdateWeightUnits unit -> { model | weightUnits = unit }
-    UpdateWeight weight -> { model | weight = weight }
+    UpdateWeight weight partial -> { model | weight = weight, partialWeight = partial }
     NoOp -> model
 
 -- View
 view : Signal.Address Action -> Model -> Html
 view address model =
-  div []
-        [ selectDiv address onChangeGender "Gender" ["Male", "Female"]
-        , selectDiv address (\v -> onChangeUnit v (\units -> UpdateBodyweightUnits <| toUnits units)) "Bodyweight Units" ["lbs", "kg"]
-        , selectDiv address (\v -> onChangeUnit v (\units -> UpdateWeightUnits <| toUnits units)) "Weight Units" ["lbs", "kg"]
-        , field "text" address UpdateBodyweight "Body Weight" <| withDefault "" <| map toString model.bodyweight
-        , field "text" address UpdateWeight "Weight" <| withDefault "" <| map toString model.weight
-        , fromElement <| show model
-        ]
+  let
+    toKg = (\x -> x * 0.453592)
+    bodyweight = if model.bodyweightUnits == Kg then model.bodyweight else map toKg model.bodyweight
+    weight = if model.weightUnits == Kg then model.weight else map toKg model.weight
+    maybeWilks = Maybe.map2 (wilksScoreKg model.gender) bodyweight weight
+    textWilks = Maybe.withDefault "" <| map toString maybeWilks
+  in
+    div []
+          [ selectDiv address onChangeGender "Gender" ["Male", "Female"]
+          , selectDiv address (\v -> onChangeUnit v (\units -> UpdateBodyweightUnits <| toUnits units)) "Bodyweight Units" ["lbs", "kg"]
+          , selectDiv address (\v -> onChangeUnit v (\units -> UpdateWeightUnits <| toUnits units)) "Weight Units" ["lbs", "kg"]
+          , field "text" address UpdateBodyweight "Body Weight" model.partialBodyweight
+          , field "text" address UpdateWeight "Weight" model.partialWeight
+          , div [] [ div [ fieldNameStyle "160px"] [ text "Wilks Score" ]
+                   , text textWilks]
+          , fromElement <| show model
+          ]
 
 
 removeSpace : String -> String
@@ -93,7 +106,7 @@ onChangeUnit address toAction =
 
 
 
-field : String -> Signal.Address Action -> (Maybe Float -> Action) -> String -> String -> Html
+field : String -> Signal.Address Action -> (Maybe Float -> String -> Action) -> String -> String -> Html
 field fieldType address toAction name content =
   div []
         [ div [ fieldNameStyle "160px" ] [ label [ attribute "for" <| removeSpace name ] [text name] ]
@@ -107,10 +120,15 @@ field fieldType address toAction name content =
             []
         ]
 
-stringToAction : Signal.Address Action -> (Maybe Float -> Action) -> String -> Signal.Message
-stringToAction address toAction string = Signal.message address (toAction (case (String.toFloat string) of
-                                                           Ok r -> Just r
-                                                           Err _ -> Nothing))
+stringToAction : Signal.Address Action -> (Maybe Float -> String -> Action) -> String -> Signal.Message
+stringToAction address toAction string =
+  case (String.toFloat string) of
+    Ok r ->
+      if String.endsWith "." string then
+        Signal.message address <| toAction (Just r) string
+      else
+        Signal.message address <| toAction (Just r) (toString r)
+    Err _ -> Signal.message address <| toAction Nothing string
 
 fieldNameStyle : String -> Attribute
 fieldNameStyle px =
@@ -120,6 +138,48 @@ fieldNameStyle px =
     , ("text-align", "right")
     , ("display", "inline-block")
     ]
+
+-- All Wilks formulas are calculated with Kg weights
+wilksScoreKg : Gender -> Float -> Float -> Float
+wilksScoreKg gender bodyweight weight =
+  (wilksCoeffKg gender bodyweight) * weight
+
+wilksCoeffKg : Gender -> Float -> Float
+wilksCoeffKg gender bodyweight =
+  case gender of
+    Male -> maleWilksCoeffKg bodyweight
+    Female -> femaleWilksCoeffKg bodyweight
+
+maleWilksCoeffKg : Float -> Float
+maleWilksCoeffKg bodyweight =
+  let
+    a = -216.0475144
+    b = 16.2606339
+    c = -0.002388645
+    d = -0.00113732
+    e = 0.00000701863
+    f = -0.00000001291
+  in
+    wilksCoeffWithCoeffs a b c d e f bodyweight
+
+femaleWilksCoeffKg : Float -> Float
+femaleWilksCoeffKg bodyweight =
+  let
+    a = 594.31747775582
+    b = -27.23842536447
+    c = 0.82112226871
+    d = -0.00930733913
+    e = 0.00004731582
+    f = -0.00000009054
+  in
+    wilksCoeffWithCoeffs a b c d e f bodyweight
+
+wilksCoeffWithCoeffs : Float -> Float -> Float -> Float -> Float -> Float -> Float -> Float
+wilksCoeffWithCoeffs a b c d e f bodyweight =
+  let
+    x = bodyweight
+  in
+    500 / (a + (b * x) + (c * (x^2)) + (d * (x^3)) + (e * (x^4)) + (f * (x^5)))
 
 main =
   StartApp.start { model = initModel, update = update, view = view }
